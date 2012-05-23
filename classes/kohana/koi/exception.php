4b76ce89ce8d271ae5d93b914300656aef26d56c
Gabriel Evans <gabriel@codeconcoction.com>
2012-05-22 23:32:51 -0700

    Add Koi_Exception class

---
+++ classes/kohana/koi/exception.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+/**
+ * Koi exception class.
+ *
+ * @package  Koi
+ */
+class Kohana_Koi_Exception extends Kohana_Exception {
+	// @see Kohana_Exception
+}
---
+++ classes/koi/exception.php
@@ -0,0 +1,5 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+class Koi_Exception extends Kohana_Koi_Exception {
+	// @see Kohana_Koi_Exception
+}
